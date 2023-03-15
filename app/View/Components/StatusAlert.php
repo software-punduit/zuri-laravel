2079679ed70bc42fb24a58a06c45d041bca9626c
software-punduit <72712348+software-punduit@users.noreply.github.com>
2023-03-15 11:15:17 +0000

    Implement email verification feature

--- app/View/Components/StatusAlert.php
+++ app/View/Components/StatusAlert.php
@@ -6,14 +6,15 @@ use Illuminate\View\Component;
 
 class StatusAlert extends Component
 {
+    // public string $value;
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(public string $value = "")
     {
-        //
+        // $this->value = $value;
     }
 
     /**
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
@@ -42,4 +42,56 @@
             </div>
         </div>
     </x-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<x-auth-layout>
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="/">
+                {{ config('app.name', 'Laravel') }}
+
+            </a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">
+                    {{ __('Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                </p>
+
+                <x-status-alert :value="'verification-link-sent'" :message="'A new verification link has been sent to the email address you provided in your profile settings.'"></x-status-alert>
+                <form action="{{ route('verification.send') }}" method="post">
+                    @csrf
+
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">
+                                {{ __('Resend Verification Email') }}
+
+                            </button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <p class="mt-3 mb-1">
+                    <a class="btn btn-link" href="{{ route('profile.show') }}">
+                        {{ __('Edit Profile') }}
+                    </a>
+                </p>
+
+                <p class="mb-1">
+                <form method="POST" action="{{ route('logout') }}" class="inline">
+                    @csrf
+
+                    <button type="submit" class="btn btn-link">
+                        {{ __('Log Out') }}
+                    </button>
+                </form>
+                </p>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+</x-auth-layout>
