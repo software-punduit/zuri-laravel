6967dd68aa9b9e5adcc5d258226ee76411a01e2b
software-punduit <72712348+software-punduit@users.noreply.github.com>
2023-03-20 10:53:39 +0000

    Modify the header and the sidebar sections of the dashboard page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,11 +36,11 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60"
+            <img class="animation__shake" src=" {{ asset('img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60"
                 width="60">
         </div>
 
-        {{$Slot}}
+        {{$slot}}
         <footer class="main-footer">
             <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
             All rights reserved.
